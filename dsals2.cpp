2a4bc66d4fbcbbaacce1ceceb4a4b41870cdc0a6
ShreyaseeVerma <shreyaseeverma76759@gmail.com>
2026-07-17 08:34:59 +0530

    Add files via upload

---
+++ dsals2.cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+// Find the largest and smallest in the array.
+#include <iostream>
+#include <climits>
+using namespace std;
+int main()
+{
+    int size=10;
+    int arr[size];
+    cout <<"enter 10 integer:" << endl;
+    for (int i=0;i<size;i++){
+        cin >> arr[i] ;
+        cout << endl;
+         }
+    int smallest=INT_MAX;
+    int largest=INT_MIN; 
+    for(int i=0;i<size;i++){
+        smallest=min(arr[i],smallest);
+        largest=max(arr[i],largest);
+       // if(arr[i]<smallest){
+         //   smallest=arr[i];
+        
+    }
+    cout << "The samllest number is:" << smallest << endl;
+    
+   /* int largest=INT_MIN; 
+    for(int i=0;i<size;i++){
+        
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+    }*/
+    cout << "The Largest number is:" << largest << endl;
+
+
+    return 0;
+}
